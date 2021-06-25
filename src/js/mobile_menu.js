f11137a48ad2c7caeb90399b8166f865deeeaa8c
Denis-Poltavskiy <82322000+Denis-Poltavskiy@users.noreply.github.com>
2021-06-26 01:24:28 +0700

    Add files via upload

--- src/js/mobile_menu.js
+++ src/js/mobile_menu.js
@@ -4,6 +4,7 @@ let main=document.querySelector('.main');
 var mobile_menu__left=document.querySelector('.mobile-menu__left');
 var burger=document.querySelector('.header__main-menu_button');
 let vacuum = document.createElement('div');
+var menu_close=document.querySelector('.mobile-menu__head_close');
 mobile_menu__left.style.height=height_mobile_menu+'px';
 burger.addEventListener("click", function () {
    mobile_menu__left.style.display="flex";
@@ -15,7 +16,14 @@ burger.addEventListener("click", function () {
 
 vacuum.addEventListener("click", function () {
   mobile_menu__left.style.display="none";
-  vacuum.classList.remove("vacuum")
+  vacuum.classList.remove("vacuum");
+  main.removeChild(vacuum);
+});
+menu_close.addEventListener("click", function () {
+  mobile_menu__left.style.display = 'none';
+  vacuum.classList.remove("vacuum");
+  main.removeChild(vacuum);
+
 });
 
 // var height_form=document.documentElement.scrollHeight;
